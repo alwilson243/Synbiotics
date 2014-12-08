76e354301fb4774a10a6ffa5eee25669f81bb4e4
andrew <alwilson243@gmail.com>
2014-12-07 23:53:01 -0800

    array of strings instead of ints

--- markup.py
+++ markup.py
@@ -7,8 +7,8 @@ For each record in dataframe
 
   file.write("<li>\n")
   file.write("Omim Record " + record.omim_id + "\n")
-  # http://www.decalage.info/en/python/print_list assuming its an array of ints
-  file.write("Associated rs_ids: " + str(record.rs_ids).strip('[]') + "\n")
+  # http://www.decalage.info/en/python/print_list assuming its an array of strings
+  file.write("Associated rs_ids: " + ", ".join(record.rs_ids) + "\n")
   file.write("Title: " + record.TI + "\n")
   file.write("Description: " + record.TX["Description"] + "\n")
   file.write("Clinical Features: " + record.TX["Clinical_Features"] + "\n")
